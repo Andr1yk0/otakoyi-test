ea8292774ee285145b713a0b9d1f06b12bf5c6ab
AndriyLozynskiy <alozynskiy@gmail.com>
2016-11-20 21:32:29 +0200

    init4

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('BASE_PATH', __DIR__);
-require_once __DIR__ .'/app/helpers.php';
-require_once __DIR__ .'/vendor/autoload.php';
-
-$app = new App\core\App();
-$app->start();
--- views/elements/_loading.php
+++
@@ -1,4 +0,0 @@
-<div class="loading text-center">
-    <p>Завантаження</p>
-    <i class="fa fa-spinner fa-pulse fa-3x" aria-hidden="true"></i>
-</div>
